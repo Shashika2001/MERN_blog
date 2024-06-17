e0ad1a64185abd54bdbdc1bc2c4b1843b83ca8bd
shashika2001 <ekanayakespm@gmail.com>
2024-06-17 23:55:25 +0530

    complete create post functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -40,9 +40,9 @@ export const signin = async (req, res , next)=>{
         }
         const token = jwt.sign(
             {
-                id: validUser._id,isAdmin: user.isAdmin}, process.env.JWT_SECRET);
+                id: validUser._id,isAdmin: validUser.isAdmin}, process.env.JWT_SECRET);
         const {password: pass, ...rest}=validUser._doc;        
-        res.status(200).cookie("Access_Token",token,{
+        res.status(200).cookie("access_token",token,{
             httpOnly: true
         }).json(rest);
 
