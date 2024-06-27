c0c89c544798c19f75af830312022aca894a3138
shashika2001 <ekanayakespm@gmail.com>
2024-06-27 18:04:09 +0530

    error in map function

--- client/src/components/DashPost.jsx
+++ client/src/components/DashPost.jsx
@@ -2,19 +2,23 @@ import { Table } from 'flowbite-react';
 import React from 'react'
 import { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom';
 
 export default function DashPost() {
   const {currentUser} = useSelector((state)=>state.user);
   const {userPosts,setUserPosts} = useState([]);
-  console.log(userPosts);
+ console.log(userPosts);
   useEffect(()=>{
     
     const fetchPosts = async () => {
       try{
-        const res = await fetch(`/api/post/getposts?userId= ${currentUser._id}`);
+        const res = await fetch(`/api/post/getposts?userId=${currentUser._id}`);
+        console.log(res);
         const data = await res.json();
+        //console.log(data);
         if(res.ok){
           setUserPosts(data.posts)
+          //console.log(data.posts);
         }
 
       }
@@ -29,15 +33,55 @@ if (currentUser.isAdmin) {
   },[currentUser._id]);
   return (
     <div>
-      {currentUser.isAdmin && userPosts.length > 0 ? (
+      {currentUser.isAdmin > 0 ? (
       <>
         <Table hoverable className= 'shadow-md'>
           <Table.Head>
+            <Table.HeadCell>Date updated</Table.HeadCell>
+            <Table.HeadCell>Post image</Table.HeadCell>
+            <Table.HeadCell>Post Title</Table.HeadCell>
+            <Table.HeadCell>category</Table.HeadCell>
+            <Table.HeadCell>Delete</Table.HeadCell>
             <Table.HeadCell>
-              Date updated
-
+              <span>Edit</span>
             </Table.HeadCell>
           </Table.Head>  
+
+          {userPosts.map((post)=>(
+            <Table.Body>
+              <Table.Row>
+                <Table.Cell>
+                  {new Date(post.updatedAt).toLocaleDateString()}
+                </Table.Cell>
+                <Table.Cell>
+                  <Link to = {`/post/${post.slug}`}>
+                    <img
+                     src={post.image}
+                      alt={post.title} 
+                      className='w-20 h-20 object-cover bg-gray-500' />
+                      </Link>
+                </Table.Cell>
+                <Table.Cell>
+                  <Link to = {`/post/${post.slug}`}>
+                    {post.title}
+                  </Link>
+                </Table.Cell>
+                <Table.Cell>
+                  {post.category}
+                </Table.Cell>
+                <Table.Cell>
+                  <span className='font-medium text-red-500 hover:underline cursor-pointer'>Delete</span>
+                </Table.Cell>
+                <Table.Cell>
+                  <Link to = {`/update/${post.slug}`}>
+                    <span className='text-teal-500 hover:underline'>Edit</span>
+                  </Link>  
+                  
+                </Table.Cell>
+                  
+              </Table.Row>
+            </Table.Body>
+          ))}
 
         </Table>
       </>
